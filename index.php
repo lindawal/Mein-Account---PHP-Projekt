38aa05cfb4c996a4604d2f374df4cb884da8b0b6
lindawal <froileinwalther@gmail.com>
2023-02-24 18:15:14 +0100

    load

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
+/* Session-Start oder Session-Wiederaufnahme */
+session_start();
+
 //Header einbinden
 include("./includes/header.inc.php");
 ?>
 
 <script>
-  //beim Klick der Radiobuttons zum Sortieren der Suchergebnisse soll sofort die Neusortierung ausgelöst werden
-  function validateRadio() {
-    document.getElementById('myForm').submit();
-  };
-  //beim Klick des Resetbuttons soll die Seite ohne die Search- & Sort-Parameter neu geladen werden
+   //beim Klick des Resetbuttons soll die Seite ohne die Search- & Sort-Parameter neu geladen werden
   function resetPage() {
     let urlObj = new URL(window.location.href);
     urlObj.search = '';
@@ -16,34 +15,31 @@ include("./includes/header.inc.php");
     location.replace(cleanUrl);
   }
 </script>
-
-
-
 <?php
 include_once("./includes/dyn_Weiterleitung.inc.php");
 
 //wenn der Parameter page gesendet wurde und auf "new_user" gesetzt ist, soll die Datei zum User Anlegen eingebunden werden
 
 //wenn der Button mit dem Namen change gesendet wurde, soll die Datei zum Ändern der Userdaten eingebunden werden
-if (isset($_GET['page'])) {
-  if ($_GET['page'] == 'new_user')
-    include("./new_user.php");
+// if (isset($_GET['page'])) {
+//   if ($_GET['page'] == 'new_user')
+//     include("./new_user.php");
 
 
-  if ($_GET['page'] == 'change_user') {
-    include("./includes/change_user.inc.php");
+//   if ($_GET['page'] == 'change_user') {
+//     include("./includes/change_user.inc.php");
 
     //   if (isset($_POST["safe_changes"])) {
 // include("./includes/change_user_db.inc.php");
 // }
-  }
-}
+//   }
+// }
 //Standartmäßig sollen die Übersicht über alle Nutzer und die Filteroptionen angezeigt werden
 if (!isset($_GET['page'])) {
   //Button um neuen User anzulegen
-  echo "<button type='submit' name='new_user'><a href='";
-  linkTo("index.php?page=new_user");
-  echo "'>Neuen Nutzer anlegen</a></button><hr>";
+  // echo "<button type='submit' name='new_user'><a href='";
+  // linkTo("index.php?page=new_user");
+  // echo "'>Neuen Nutzer anlegen</a></button><hr>";
 
   /**  ANCHOR Filteroptionen
    ***/
@@ -53,9 +49,9 @@ if (!isset($_GET['page'])) {
     $search_input = htmlentities($_GET["search"]);
   }
 
-  $searchArea_input = "lastname"; //Suchbereich Default-Wert
-  if (isset($_GET["searchArea"])) {
-    $searchArea_input = $_GET["searchArea"];
+  $search_area = "lastname"; //Suchbereich Default-Wert
+  if (isset($_GET["search_area"])) {
+    $search_area = $_GET["search_area"];
   }
 
   $sort = "lastname"; //Sortierung Default-Wert
@@ -65,57 +61,75 @@ if (!isset($_GET['page'])) {
 
   //Formular zum Angeben der Suchparameter
   echo "<form action='index.php' method='get' class='myForm'>
-    <input type='radio' name='searchArea' value='lastname'> Nachname
-    <input type='radio' name='searchArea' value='firstname'> Vorname
-    <input type='radio' name='searchArea' value='username'> Username
-    <input type='radio' name='searchArea' value='mail'> E-Mail<br>
-    Suche: <input name='search' value='" . $search_input . "'>
-    <input type='submit' class='sort_button'>
-    <input type='button' onClick='resetPage();' value='reset' class='sort_button'>
-  <hr>" .
-    // Radiobuttons für die Sortierung
-    // Wenn diese angeklickt werden, wird die Änderung mit Hilfe eines Javascript onClick Events sofort ausgelöst
-    // werden mit Hilfe einer If-Anweisung auf "checked" gesetzt
-    "<label for='sort'>Sortierung:</label>
-    <select id='sort' name='sort' onchange='this.form.submit()'>
-    <option value='lastname'";
-  // if ($sort == "lastname") {
-  //   echo " checked='checked'";
-  // }
-  echo ">vorname </option>
-         <option value='firstname'";
-  // if ($sort == "firstname") {
-  //   echo " checked='checked'";
-  // }
-  echo ">nachname </option></select>
-      
-        Username <input type='radio' name='sort' value='username' onClick='validateRadio();'";
-  if ($sort == "username") {
+  <div class='filter_box'>
+      <label class='my-radio'>
+      <p><input type='radio' name='search_area' value='lastname'";
+  if ($search_area == "lastname") {
+  echo " checked='checked'";
+  }
+  echo "> Nachname</p>
+      <p><input type='radio' name='search_area' value='firstname'";
+  if ($search_area == "firstname") {
     echo " checked='checked'";
   }
-  echo ">
-      
-        <h3>E-Mail</h3><input type='radio' name='sort' value='mail' onClick='validateRadio();'";
-  if ($sort == "mail") {
+  echo "> Vorname</p>
+      <p><input type='radio' name='search_area' value='username'";
+  if ($search_area == "username") {
     echo " checked='checked'";
   }
-  echo ">
+  echo "> Username</p>
+      <p><input type='radio' name='search_area' value='mail'";
+  if ($search_area == "mail") {
+    echo " checked='checked'";
+  }
+  echo "> E-Mail</p><br></label>
+      Suche: <input type='text' name='search' value='" . $search_input . "'>
+      <input type='submit' class='sort_button'>
+      <input type='button' onClick='resetPage();' value='reset' class='sort_button'></div>";
 
-      
-   </form>
-  <div class='all-users'>
-  ";
+      // Selectfelder für die Sortierung
+      // Wenn diese angeklickt werden, wird die Änderung mit Hilfe eines Javascript onchange Events sofort ausgelöst
+      // die aktive Option wird mit Hilfe einer If-Anweisung auf "selected" gesetzt
+  echo "<label for='sort'>Sortierung:</label>
+      <select id='sort' name='sort' onchange='this.form.submit()'>
+      <option value='firstname'";
+        if ($sort == "firstname") {
+        echo " selected";
+        }
+      echo ">Vorname </option>
+
+      <option value='lastname'";
+        if ($sort == "lastname") {
+        echo " selected";
+        }
+      echo ">Nachname </option>
+        
+      <option value='username'";
+        if ($sort == "username") {
+        echo " selected";
+        }
+      echo ">Username</option>
+  
+      <option value='mail'";    
+        if ($sort == "mail") {
+          echo " selected";
+        }
+      echo ">E-Mail</option>
+      </select>
+      </form>";
 
   //Datei einbinden um Daten aus der Datenbank zu laden ->
 //als Variablen werden die Suchparameter gesendet
-  include("./includes/load_users_db.inc.php");
-  load_users_db(
-    "logindaten_neu",
-    "user",
-    $searchArea_input,
+  include("./includes/class_all_users.inc.php");
+  $all_users = new All_Users(0);
+  $all_users->load_with_filter(
+    $search_area,
     $sort,
     $search_input
   );
+  echo "Ergebnis: " . $all_users->data_length .
+    "<div class='all-users'>";
+  $all_users->show_as_grid();
 }
 ?>
 
