3e0394ae4af8d225e3be0c9e8b142156072c7a16
lindawal <froileinwalther@gmail.com>
2023-03-10 15:37:06 +0100

    new version

--- admin.php
+++ admin.php
@@ -9,48 +9,56 @@ include_once("./includes/dyn_Weiterleitung.inc.php");
 
 
 if (isset($_SESSION["name"])) { //prüfen, ob eine benannte Session vorhanden ist
+  if ($_SESSION["admin"] == true) {
+    //Begrüßung
 
-  //Begrüßung
+    echo "Hallo " . $_SESSION["firstname"] . " " . $_SESSION["lastname"] . "!<br>";
 
-  echo "Hallo " . $_SESSION["firstname"] . " " . $_SESSION["lastname"] . ",<br>hier siehst du deine persönlichen Inhalte:";
-  // echo "<br><br>Host: " . $_SERVER['HTTP_HOST'] . "<br>"
-  //   . "Request URI: " . $_SERVER['REQUEST_URI'] . "<br>"
-  //   . "Script Name: " . $_SERVER['SCRIPT_NAME'] . "<br>"
-  //  . "Query String: " . $_SERVER['QUERY_STRING'] . "<br>"
-  //  . "referer: " . $_SERVER['HTTP_REFERER'] . "<br>"
-  //  . "Self " . $_SERVER['PHP_SELF'] . "<br>";
-  //define("last_uri", $_SERVER['SCRIPT_NAME']);
-  //$uri = rtrim($_SERVER['HTTP_REFERER']);
- // $page = rtrim(dirname($_SERVER['SCRIPT_NAME']), $uri);
-  //echo "last_uri: " . last_uri . "<br><br>";
-  
+    //wenn der Parameter page gesendet wurde und auf "new_user" gesetzt ist, soll die Datei zum User Anlegen eingebunden werden
+    if (isset($_GET['page'])) {
+      if ($_GET['page'] == 'new_user')
+        include_once("./includes/admin_new_user.inc.php");
 
-  //wenn der Parameter page gesendet wurde und auf "new_user" gesetzt ist, soll die Datei zum User Anlegen eingebunden werden
-  if (isset($_GET['page'])) {
-    if ($_GET['page'] == 'new_user')
-      include_once("./includes/new_user.inc.php");
+      //wenn der Button mit dem Namen change gesendet wurde, soll die Datei zum Ändern der Userdaten eingebunden werden
+      if ($_GET['page'] == 'change_user') {
+        if (isset($_POST["change"])) {
+          safe_filter($_SERVER['HTTP_REFERER']);
+        }
+        include_once("./includes/admin_edit_user.inc.php");
 
-    //wenn der Button mit dem Namen change gesendet wurde, soll die Datei zum Ändern der Userdaten eingebunden werden
-    if ($_GET['page'] == 'change_user') {
-    if (isset($_POST["change"])) {
-    //include_once("./includes/suchparameter.php");
-        safe_filter($_SERVER['HTTP_REFERER']);
+      }
+      if ($_GET['page'] == 'delete_user') {
+        if (isset($_POST["confirm_del_user"])) {
+        }
+        include_once("./includes/admin_delete_user.inc.php");
+
+      }
+      if ($_GET['page'] == 'my_data') {
+        if (isset($_POST["change"])) {
+        }
+        include_once("./includes/edit_user.inc.php");
+
+      }
+      if ($_GET['page'] == 'delete_my_account') {
+        if (isset($_POST["confirm_del_user"])) {
+        }
+        include_once("./includes/delete_user.inc.php");
+
+      }
     }
-      include_once("./includes/change_user.inc.php");
-        
+    //Standartmäßig sollen die Übersicht über alle Nutzer und die Filteroptionen angezeigt werden
+    if (!isset($_GET['page'])) {
+      include_once("./includes/admin_interface.inc.php");
     }
-  }
-  //Standartmäßig sollen die Übersicht über alle Nutzer und die Filteroptionen angezeigt werden
-   if (!isset($_GET['page'])) {
-     include_once("./includes/admin_interface.inc.php");
-   }
+  } else
+    weiterleiten("user.php");
+
 } else {
   /***
   Wenn noch kein Login statt fand, dann Weiterleitung zur Login-Seite
   ***/
-  if (!isset($_GET['page'])) {
-    weiterleiten("index.php?page=login");
-  }
+  //if (!isset($_GET['page'])) {
+  weiterleiten("index.php?page=login");
 }
 ?>
 
--- index.php
+++ index.php
@@ -4,69 +4,40 @@ session_start();
 
 //Header einbinden
 include("./includes/header.inc.php");
-?>
 
-<script>
-   //beim Klick des Resetbuttons soll die Seite ohne die Search- & Sort-Parameter neu geladen werden
-  function resetPage() {
-    let urlObj = new URL(window.location.href); //URL holen
-    urlObj.search = ''; //Parameter löschen
-    let cleanUrl = urlObj.toString(); //URL in STring umwandeln
-    location.replace(cleanUrl);
-  }
-</script>
-<?php
 include_once("./includes/dyn_Weiterleitung.inc.php");
 
 
 if (isset($_SESSION["name"])) { //prüfen, ob eine benannte Session vorhanden ist
- if (isset($_GET['page'])) {
-    if ($_GET['page'] == 'login_area') {
-      weiterleiten("index.php");
-    }
-   }
 
-/***
-  Begrüßung
- ***/
-
-  echo "Hallo " . $_SESSION["firstname"] . " " . $_SESSION["lastname"] . ",<br>hier siehst du deine persönlichen Inhalte:";
-
-
-  //wenn der Parameter page gesendet wurde und auf "new_user" gesetzt ist, soll die Datei zum User Anlegen eingebunden werden
   if (isset($_GET['page'])) {
-    if ($_GET['page'] == 'new_user')
-      include_once("./new_user.php");
-
-    //wenn der Button mit dem Namen change gesendet wurde, soll die Datei zum Ändern der Userdaten eingebunden werden
-    if ($_GET['page'] == 'change_user') {
-      include_once("./includes/change_user.inc.php");
-
-      // if (isset($_POST["safe_changes"])) {
-// include("./includes/change_user_db.inc.php");
-// }
+    if (($_GET['page'] == 'login') || ($_GET['page'] == 'signup')) {
+      weiterleiten("index.php"); //dynamische Weiterleitung
     }
   }
-  //Standartmäßig sollen die Übersicht über alle Nutzer und die Filteroptionen angezeigt werden
-  if (!isset($_GET['page'])) {
-    include_once("./includes/admin_interface.inc.php");
-  }
+  if ($_SESSION["admin"] == true) {
+    weiterleiten("admin.php");
+  } else
+    weiterleiten("user.php");
+
 } else {
   /***
   Wenn noch kein Login statt fand, dann Weiterleitung zur Login-Seite
   ***/
   if (!isset($_GET['page'])) {
-    weiterleiten("index.php?page=login_area");
+    weiterleiten("index.php?page=login");
   }
   if (isset($_GET['page'])) {
-    if ($_GET['page'] == 'login_area') {
-      include("./includes/login_area.inc.php");
+    if ($_GET['page'] == 'login') {
+      include("./includes/login.inc.php");
     }
-    if ($_GET['page'] == 'register_area') {
-      include("./includes/register_area.inc.php");
+    if ($_GET['page'] == 'password_reset') {
+      include("./includes/change_pw.inc.php");
+    }
+    if ($_GET['page'] == 'signup') {
+      include("./includes/signup.inc.php");
     }
   }
-
 }
 ?>
 
