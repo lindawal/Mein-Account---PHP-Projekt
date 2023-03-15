addea5a6daaed320cc5c1fd1c00ee0a6979d5a62
lindawal <froileinwalther@gmail.com>
2023-03-15 08:39:35 +0100

    wird nicht mehr benötigt

--- js/admin.js
+++
@@ -1,8 +0,0 @@
-
-//beim Klick des Resetbuttons soll die Seite ohne die Search- & Sort-Parameter neu geladen werden
-function resetPage() {
-  let urlObj = new URL(window.location.href); //URL holen
-  urlObj.search = ''; //Parameter löschen
-  let cleanUrl = urlObj.toString(); //URL in STring umwandeln
-  location.replace(cleanUrl);
-}
