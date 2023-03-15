5ec10ba1b2dce237868b125d5b54d59a838d32a3
lindawal <froileinwalther@gmail.com>
2023-03-15 09:09:29 +0100

    angepasst

--- includes/database.config.php
+++ includes/database.config.php
@@ -1,13 +1,5 @@
 <?PHP
 
-// Zum Aufbau der Verbindung zur Domain-Datenbank
-
-//  define('MYSQL_HOST', 'localhost');
-//  define('MYSQL_USER', 'admin_680929');
-//  define('MYSQL_PASSWORD', 'QDkXB+edz2Fgtu');
-//  define('MYSQL_DATABASE', 'db680929_1');
-
-
 // Zum Aufbau der Verbindung zur lokalen (Xampp) Datenbank
 
  define('MYSQL_HOST', 'localhost');
