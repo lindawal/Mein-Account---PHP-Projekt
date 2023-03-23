d30765d66bf9c8a5e0567c50e7232c75634efa40
lindawal <froileinwalther@gmail.com>
2023-03-23 10:55:41 +0100

    neu

---
+++ database.config.php
@@ -0,0 +1,10 @@
+<?PHP
+
+// Zum Aufbau der Verbindung zur lokalen (Xampp) Datenbank
+
+ define('MYSQL_HOST', 'localhost');
+ define('MYSQL_USER', 'root');
+ define('MYSQL_PASSWORD', '');
+ define('MYSQL_DATABASE', 'portfolio_gallery');
+
+?>
